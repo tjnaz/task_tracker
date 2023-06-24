c38e57ebc571815b708da393a6346b36d457b166
Thaj Anaz <anaz.thaj@gmail.com>
2023-06-24 18:20:40 +0530

    add: button with events fired onClick

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,14 @@ import PropTypes from "prop-types";
 import Button from "./Button";
 
 const Header = ({ title }) => {
+  const onClick = () => {
+    console.log("click");
+  };
+
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button />
+      <Button color="green" text="Add" onClick={onClick} />
     </header>
   );
 };
