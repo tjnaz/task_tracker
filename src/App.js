d73d6626c7e863b1a56cb85097a06ef2828680d7
Thaj Anaz <anaz.thaj@gmail.com>
2023-06-24 23:43:42 +0530

    add: import addTask component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 function App() {
   const name = "tj";
@@ -26,6 +27,11 @@ function App() {
     },
   ]);
 
+  // Add task
+  const addTask = (task) => {
+    console.log(task);
+  };
+
   // Delete task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
@@ -43,6 +49,7 @@ function App() {
   return (
     <div className="container">
       <Header />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
