c3ce9bc79c1e9bdba34e13c4cc9d84f784324c98
Thaj Anaz <anaz.thaj@gmail.com>
2023-06-25 13:33:49 +0530

    feat: delete the tasks from the server

--- src/App.js
+++ src/App.js
@@ -14,7 +14,11 @@ function App() {
   };
 
   // Delete task
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "DELETE",
+    });
+
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
